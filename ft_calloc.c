d00a2dc88d4f6a39231285c297a86914ecebc8ff
valedictum <138375821+valedictum@users.noreply.github.com>
2023-07-18 23:04:23 +0930

    calloc + memchr again

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,4 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ft_calloc.c
+#include <stdlib.h>
+void *calloc(size_t nmemb, size_t size);
+
+Description
+The calloc() function allocates memory for an array of nmemb elements of size 
+bytes each and returns a pointer to the allocated memory. The memory is set to 
+zero. If nmemb or size is 0, then calloc() returns either NULL, or a unique 
+pointer value that can later be successfully passed to free(). 
+*/
+
 #include "libft.h"
+
+void    *ft_calloc(size_t nmemb, size_t size)
+{
+    void    *memory;
+    memory = malloc(nmemb * size);
+    
+    if (memory != NULL)
+    {
+        memset(memory, 0, nmemb * size);
+    }  
+    return (memory);
+}
+
+int main()
+{
+    int nmemb = 5;
+    int size = sizeof(int);
+    
+    int *array = (int*)ft_calloc(nmemb, size);
+    
+    if (array == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return (1);
+    }
+    for (int i = 0; i < nmemb; i++)
+    {
+        array[i] = i + 1;
+        printf("%d", array[i]);
+    }
+    free(array);
+    return (0);
+}
--- ft_memchr.c
+++ ft_memchr.c
@@ -10,4 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h" 
+/*
+ft_memchr.c
+#include <string.h>
+void *memchr(const void *s, int c, size_t n);
+
+DESCRIPTION
+The memchr() function locates the first occurrence of c (converted to an
+unsigned char) in string s.
+
+RETURN VALUES
+The memchr() function returns a pointer to the byte located, or NULL if
+no such byte exists within n bytes.
+*/
+
+#include "libft.h"
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *p = s;
+    unsigned char target = (unsigned char)c;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        if (p[i] == target)
+        {
+            return (void *)(p + i);
+        }
+    }
+    return (NULL);
+}
+
+int main()
+{
+    char buffer[] = "Hello World!";
+    char ch = 'W'
+    char *result = ft_memchr(buffer, ch, strlen(buffer));
+
+    if (result != NULL)
+    {
+        printf("Found '%c' at position %ld\n", ch, result - buffer);
+    }
+    else
+    {
+        printf("Character not found.\n");
+    }
+    return (0);
+}
