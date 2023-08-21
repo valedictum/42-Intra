5e058844f1141d71111784c50a20f2df37da9dc9
Andrew Tang <atang@F2N072.42adel.org.au>
2023-08-21 16:53:04 +0930

    addGNL

---
+++ w-get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atang <atang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 14:49:29 by atang             #+#    #+#             */
+/*   Updated: 2023/08/21 15:03:05 by atang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+int		ft_printf(const char *string, ...);
+
+#endif
+
