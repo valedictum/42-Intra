8e3f86b4dacdb982763f887dea381c68d29d38a1
Andrew Tang <atang@F2K010.42adel.org.au>
2024-02-04 20:00:55 +1030

    2024 Feb 04/02/24 8.00pm - Added from 42

---
+++ PS-cloned-Feb2-7.28pm/srcs/push_swap/ps_find_utils.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ps_find_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atang <atang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/29 10:31:51 by atang             #+#    #+#             */
+/*   Updated: 2023/09/29 10:53:00 by atang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+size_t	ps_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	ps_stack_size(t_stack_node	*stack)
+{
+	size_t	stack_size;
+
+	stack_size = 0;
+	while (stack)
+	{
+		stack = stack->next;
+		stack_size++;
+	}
+	return (stack_size);
+}
+
+int	get_max_rank(t_stack_node *stack)
+{
+	int	max_rank;
+
+	if (!stack)
+		return (-1);
+	max_rank = stack->rank;
+	while (stack)
+	{
+		if (stack->rank > max_rank)
+			max_rank = stack->rank;
+		stack = stack->next;
+	}
+	return (max_rank);
+}
+
+t_stack_node	*find_element_with_rank(t_stack_node **stack, int target_rank)
+{
+	t_stack_node	*current;
+
+	current = *stack;
+	while (current != NULL)
+	{
+		if (current->rank == target_rank)
+		{
+			return (current);
+		}
+		current = current->next;
+	}
+	return (NULL);
+}
