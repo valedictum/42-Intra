de43afa789beab84fbed015bd353a9ba9362f25a
valedictum <138375821+valedictum@users.noreply.github.com>
2024-05-24 17:17:05 +0930

    2024 May 24.05.2024 5.16pm - Added from 42

---
+++ Exam03.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include <stdarg.h>
+
+void	put_string(char	*str, int	*len)
+{
+	if (!str)
+		str = "(null)";
+	while (*str)
+	{
+		*len += write(1, str, 1);
+		str++;
+	}
+}
+
+void	put_digit(long long int num, int base, int *len)
+{
+	char *hexadecimal = "0123456789abcdef";
+
+	if (num < 0)
+	{
+		num *= -1;
+		*len += write(1, '-', 1);
+	}
+	if (num >= base)
+		put_digit((num / base), base, len);
+	*len += write (1, &hexadecimal[num % base], 1);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int	len = 0;
+
+	va_list pointer;
+	va_start(pointer, format);
+
+	while (*format)
+	{
+		if ((*format == '%') && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x'))) 
+		{
+			format++;
+			if (*format) == 's')
+				put_string(va_arg(pointer, char *), &len);
+			else if (*format == 'd')
+				put_digit((long long int)va_arg(pointer, int), 10, &len);
+			else if (*format == 'x')
+				put_digit((long long int)va_arg(pointer, int), 16, &len);
+		}
+		else
+			len += write(1, format, 1);                 
+		format++;
+	}
+	return (va_end(pointer), len);
+}
