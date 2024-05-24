779eb8ba064dde9b9148e4905adc7fcda7308b58
valedictum <138375821+valedictum@users.noreply.github.com>
2024-05-24 17:54:24 +0930

    2024 May 24.05.2024 5.53pm - Added from 42

--- Exam03.c
+++ Exam03.c
@@ -43,7 +43,7 @@ int	ft_printf(const char *format, ...)
 			else if (*format == 'd')
 				put_digit((long long int)va_arg(pointer, int), 10, &len);
 			else if (*format == 'x')
-				put_digit((long long int)va_arg(pointer, int), 16, &len);
+				put_digit((long long int)va_arg(pointer, unsigned int), 16, &len);
 		}
 		else
 			len += write(1, format, 1);                 
