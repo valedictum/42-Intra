bd83d2c64f90442dd7f9fa8c91406628758645b0
valedictum <138375821+valedictum@users.noreply.github.com>
2024-05-26 18:31:31 +0930

    2024 May 26.05.2024 6.31pm - Added from 42

---
+++ success/Exam03/get_next_line/get_next_line.h
@@ -0,0 +1,15 @@
+/* get_next_line.h */
+
+/* #ifndef */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+/* includes */
+# include <unistd.h>
+# include <stdlib.h>
+
+/* prototype */
+char*get_next_line(int fd);
+
+/* endif */
+#endif
