8601e279ad24634ca70a7f1baabb33088f83cbda
valedictum <138375821+valedictum@users.noreply.github.com>
2024-08-25 18:56:16 +0930

    2024 August 25.08.2024 6.56pm - Added from 42

---
+++ minirt-250824/src/main/main.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atang <atang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/03 13:29:18 by atang             #+#    #+#             */
+/*   Updated: 2024/08/17 17:34:52 by atang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+int main(int argc, char **argv)
+{
+	//t_mrt	*mrt;
+	//t_data	*data;
+	Scene	scene;
+	//int		parse_result;
+
+	//mrt = NULL;
+	//data = NULL;
+	//i = 0;
+	if (argc != 2 || filename_error(argv[1]))
+		printf(RED "Error!\n" RST G "Correct usage is: ./mini_rt <filename>.rt\n" RST);
+	else if (file_exists(argv[1]))
+	{
+		printf(G "\nFile found...\n" RST);
+
+		int	empty_check = file_empty(argv[1]); 
+		if (empty_check == 1 || empty_check == -1)
+		{
+			return (1);
+		}
+		parse_rt_file(argv[1], &scene);
+		//data = (t_mrt *)malloc(sizeof(t_mrt));
+		//if (data == NULL)
+		//	return (1);
+		//init_mrt(mrt);
+	}
+	else
+	{
+		printf(RED "Error! File not found\n" RST);
+		return (1);
+	}
+	return (0);
+}
